2f8f5817870dab123c54819ee6b8e50bb31db18d
commelectronicja <commelectronicja@gmail.com>
2026-04-12 09:01:48 -0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,23 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
-    <div>
+    <div className="pb-16"> {/* prevents content hiding behind nav */}
       <Outlet />
 
       <nav className="fixed bottom-0 w-full bg-black flex justify-around p-3 text-white">
-        <Link to="/">Home</Link>
-        <Link to="/create">Post</Link>
-      </nav>
-    </div>
-  );
-}
+        <Link
+          to="/"
+          className={`${
+            location.pathname === "/" ? "text-purple-400" : ""
+          }`}
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/create"
+          className={`${
+            location.pathname === "/create" ? "
