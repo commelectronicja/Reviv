a6f5809e47103edfcc90543c06a7b5c6043ad4b9
commelectronicja <commelectronicja@gmail.com>
2026-04-13 22:25:30 -0500

    Update CreatePost.jsx

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { base44 } from "@/api/base44Client";
+import client from "../api/base44Client";
 import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
